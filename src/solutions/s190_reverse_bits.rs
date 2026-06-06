2b57c7f0636bbcd07814809474e5233ba9dceace
yoshiyuki-140 <moyashikuro1225@gmail.com>
2026-06-06 14:15:50 +0900

    sol: 最適化された解答を追加

--- src/solutions/s190_reverse_bits.rs
+++ src/solutions/s190_reverse_bits.rs
@@ -1,5 +1,6 @@
 pub struct Solution;
 
+#[cfg(any())]
 impl Solution {
     pub fn reverse_bits(n: i32) -> i32 {
         // 符号なし32ビットに型変換する
@@ -21,23 +22,31 @@ impl Solution {
     }
 }
 
-/* 別解 (https://leetcode.com/problems/reverse-bits/solutions/7582521/rust-with-4bit-lookup-optimization-and-s-g8yx/)
+// 別解 (https://leetcode.com/problems/reverse-bits/solutions/7582521/rust-with-4bit-lookup-optimization-and-s-g8yx/)
 // 4bit optimization means 16 bytes for lookup
 // can implement 8bit with 256 lookup
 impl Solution {
     pub fn reverse_bits(n: i32) -> i32 {
-        let lookup:[u8;16] = [0,8,4,12,2,10,6,14,1,9,5,13,3,11,7,15];
+        // ルックアップテーブルの定義
+        // 反転したBitテーブルを作成する
+        let lookup: [u8; 16] = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15];
+        // 1バイト(8bit)ずつ4つに分離
         let mut res: [u8; 4] = n.to_le_bytes();
 
+        // 可変参照でresの中身を取り出す
+        // ここでbはポインタ
         for b in &mut res {
-            let lo = *b & 15;
-            let hi = *b >> 4;
+            let lo = *b & 0b1111; // 下位4bitを取り出す
+            let hi = *b >> 4; // 上位4ビットを取り出す
+            // 合体させている
+            // lookupで反転したビットを取り出す
+            // 下位4bitは左シフトして上位4bitとして配置し
+            // 上位4bitはそのまま結合させる
             *b = (lookup[lo as usize] << 4) | lookup[hi as usize];
         }
-        return i32::from_be_bytes(res);
+        i32::from_be_bytes(res)
     }
 }
- */
 
 #[cfg(test)]
 mod tests {
