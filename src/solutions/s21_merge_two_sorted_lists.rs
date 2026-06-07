9b2c90bf8706c9b20a612ad09a0b3a45a0dbb149
yoshiyuki-140 <moyashikuro1225@gmail.com>
2026-06-07 16:57:27 +0900

    sol: 21の解答を追加

--- src/solutions/s21_merge_two_sorted_lists.rs
+++ src/solutions/s21_merge_two_sorted_lists.rs
@@ -2,6 +2,7 @@ pub struct Solution;
 use crate::common::ListNode;
 
 // https://leetcode.com/problems/merge-two-sorted-lists/solutions/2947855/simple-and-efficient-rust-8-liner-by-ral-kso9/
+#[cfg(any())]
 impl Solution {
     pub fn merge_two_lists(
         mut list1: Option<Box<ListNode>>,
@@ -21,5 +22,25 @@ impl Solution {
         }
         // list1の最初のメモリ番地を返却
         list1
+    }
+}
+
+impl Solution {
+    pub fn merge_two_lists(
+        mut li1: Option<Box<ListNode>>,
+        mut li2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        // リストの先頭のメモリ番地はlist1に残しておく
+        let mut r = &mut li1;
+        // 値がSomeかどうかを判定する
+        while li2.is_some() {
+            // rがNone,もしくはli1の値の方が大きかった場合
+            if r.is_none() || r.as_ref()?.val < r.as_ref()?.val {
+                std::mem::swap(r, &mut li2);
+            }
+            // メモリ番地を次に更新
+            r = &mut r.as_mut()?.next;
+        }
+        li1
     }
 }
